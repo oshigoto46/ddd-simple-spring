c50bfab01adf39a32c4f5297b39e7d05aff4ba90
oshigoto46 <ppp.shiro46@gmail.com>
2021-07-03 18:32:26 +0300

    TodoMapper->TodoDao rename including xml name

--- src/main/java/com/todo/app/controller/TodoController.java
+++ src/main/java/com/todo/app/controller/TodoController.java
@@ -9,21 +9,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.todo.app.entity.Todo;
-import com.todo.app.dao.TodoMapper;
+import com.todo.app.dao.TodoDao;
 
 @Controller
 public class TodoController {
 
 	@Autowired
-	TodoMapper todoMapper;
+	TodoDao todoDao;
 
 	@RequestMapping(value="/")
 	public String index(Model model) {
 
 //		List<Todo> list = todoMapper.selectAll();
 
-		List<Todo> list = todoMapper.selectIncomplete();
-		List<Todo> doneList = todoMapper.selectComplete();
+		List<Todo> list = todoDao.selectIncomplete();
+		List<Todo> doneList = todoDao.selectComplete();
 		model.addAttribute("todos",list);
 		model.addAttribute("doneTodos",doneList);
 
@@ -33,20 +33,20 @@ public class TodoController {
 	@RequestMapping(value="/add")
 	@ResponseBody
 	public Todo add(Todo todo) {
-		todoMapper.add(todo);
+		todoDao.add(todo);
 		return todo;
 	}
 
 	@RequestMapping(value="/update")
 	@ResponseBody
 	public void update(Todo todo) {
-		todoMapper.update(todo);
+		todoDao.update(todo);
 	}
 
 	@RequestMapping(value="/delete")
 	@ResponseBody
 	public void delete() {
-		todoMapper.delete();
+		todoDao.delete();
 	}
 
 }
